f1d1f73231c0cf9fc146db4f28aa3de2d119c2c3
Kacper <kacper.piotrowski1711@gmail.com>
2026-04-15 22:12:01 +0200

    Add room DTOs

---
+++ HotelAPI/HotelAPI/DTOs/CreateRoomDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelAPI.DTOs;
+
+public class CreateRoomDto
+{
+    [MaxLength(100),  Required]
+    public string Name { get; set; } = string.Empty;
+    public int BuildingCode {get; set;}
+    public int Floor { get; set; }
+    public int Capacity { get; set; }
+    public bool HasProjector { get; set; }
+    public bool IsActive { get; set; }
+}
