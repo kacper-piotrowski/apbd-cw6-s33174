8a7d0c6d0aca04e5cc91a47132eaf07af75f83ae
Kacper <kacper.piotrowski1711@gmail.com>
2026-04-15 22:42:05 +0200

    Add Reservation DTOs

---
+++ HotelAPI/HotelAPI/DTOs/CreateReservationDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HotelAPI.Models;
+
+namespace HotelAPI.DTOs;
+
+public class CreateReservationDto
+{
+    public int RoomId { get; set; }
+    [MaxLength(100),  Required]
+    public string OrganizerName { get; set; } = string.Empty;
+    [MaxLength(100),  Required]
+    public string Topic {get; set;} = string.Empty;
+    public DateTime Date {get; set;}
+    public TimeOnly StartTime {get; set;}
+    public TimeOnly EndTime {get; set;}
+    public ReservationStatus Status {get; set;}
+}
