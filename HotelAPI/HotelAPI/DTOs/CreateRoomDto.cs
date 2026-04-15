9fac31eadfda59f7d99f6bf3e5b1e30af2cd9dce
Kacper <kacper.piotrowski1711@gmail.com>
2026-04-15 23:34:56 +0200

    Update DTOs

--- HotelAPI/HotelAPI/DTOs/CreateRoomDto.cs
+++ HotelAPI/HotelAPI/DTOs/CreateRoomDto.cs
@@ -6,9 +6,14 @@ public class CreateRoomDto
 {
     [MaxLength(100),  Required]
     public string Name { get; set; } = string.Empty;
+    [Required, Range(1, int.MaxValue)]
     public int BuildingCode {get; set;}
+    [Required, Range(1, int.MaxValue)]
     public int Floor { get; set; }
+    [Range(1, int.MaxValue)]
     public int Capacity { get; set; }
+    [Required]
     public bool HasProjector { get; set; }
+    [Required]
     public bool IsActive { get; set; }
 }
--- HotelAPI/HotelAPI/DTOs/UpdateReservationDto.cs
+++ HotelAPI/HotelAPI/DTOs/UpdateReservationDto.cs
@@ -5,13 +5,18 @@ namespace HotelAPI.DTOs;
 
 public class UpdateReservationDto
 {
+    [Required, Range(1, int.MaxValue)]
     public int RoomId { get; set; }
     [MaxLength(100),  Required]
     public string OrganizerName { get; set; } = string.Empty;
     [MaxLength(100),  Required]
     public string Topic {get; set;} = string.Empty;
+    [Required]
     public DateTime Date {get; set;}
+    [Required]
     public TimeOnly StartTime {get; set;}
+    [Required]
     public TimeOnly EndTime {get; set;}
+    [Required]
     public ReservationStatus Status {get; set;}
 }
